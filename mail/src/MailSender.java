40b8105c703e522b4fe0b3139985cc3f0ae05f85
psy0525 <shineeluv525@gmail.com>
2024-08-19 19:15:49 +0900

    메일

---
+++ mail/src/MailSender.java
@@ -0,0 +1,13 @@
+import java.util.Properties;
+
+public class MailSender {
+    public static void main(String[] args) {
+        String title="메일보내기 test 메일";
+        String content="메일 보애기 test 메일 입니다.";
+        String user_name="psy19953126@naver.com";
+        String password="3FS1EQCW38QG";
+
+        SendMail sendMail = new SendMail();
+        sendMail.goMail(sendMail.setting(new Properties(), user_name, password), title, content);
+    }
+}
---
+++ mail/src/SendMail.java
@@ -0,0 +1,60 @@
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+    final String ENCODING = "UTF-8";
+    final String PORT ="465";
+    final String SMTPHOST = "smtp.naver.com";
+    final String TO = "dudtjs1902@naver.com";
+
+    public Session setting(Properties props, String user_name, String password) {
+        Session session = null;
+        try{
+            props.put("mail.transport.protocol", "smtp");
+            props.put("mail.smtp.host", SMTPHOST);
+            props.put("mail.smtp.port", PORT);
+            props.put("mail.smtp.auth", true);
+            props.put("mail.smtp.ssl.enable", true);
+            props.put("mail.smtp.ssl.trust", SMTPHOST);
+            props.put("mail.smtp.starttls.required", true);
+            props.put("mail.smtp.starttls.enable", true);
+            props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+            props.put("mail.smtp.quit-wait", "false");
+            props.put("mail.smtp.socketFactory.port", PORT);
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+
+            session = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(user_name, password);
+                }
+            });
+        }catch (Exception e){
+            System.out.println("Session Setting 실패");
+        }
+        return  session;
+
+    }
+
+    public void goMail(Session session, String title, String content){
+        Message msg = new MimeMessage(session);
+
+        try{
+            msg.setFrom(new InternetAddress("psy19953126@naver.com", "관리자", ENCODING));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+            msg.setSubject(title);
+            msg.setContent(content, "text/html; charset=utf-8");
+
+            Transport.send(msg);
+            System.out.println("메일보내기 성공");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("메일 보내기 실패");
+        }
+    }
+}
