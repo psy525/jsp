d9be450309a5ffa2fdd7252339dda1ff90500e93
psy0525 <shineeluv525@gmail.com>
2024-08-21 20:40:19 +0900

    첨부파일

--- mail/src/MailSender.java
+++ mail/src/MailSender.java
@@ -3,7 +3,7 @@ import java.util.Properties;
 public class MailSender {
     public static void main(String[] args) {
         String title="안녕하세요";
-        String content="메일 보애기 test 메일 입니다.";
+        String content="선생님 제가 제일 좋아하는 게임 캐릭터에요. 귀여죠?";
         String user_name="psy19953126@naver.com";
         String password="3FS1EQCW38QG";
 
--- mail/src/SendMail.java
+++ mail/src/SendMail.java
@@ -2,13 +2,15 @@ import java.util.Properties;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class SendMail {
     final String ENCODING = "UTF-8";
     final String PORT ="465";
     final String SMTPHOST = "smtp.naver.com";
-    final String TO = "dudtjs1902@naver.com";
+    final String TO = "psy19953126@naver.com";
 
     public Session setting(Properties props, String user_name, String password) {
         Session session = null;
@@ -22,12 +24,10 @@ public class SendMail {
             props.put("mail.smtp.starttls.required", true);
             props.put("mail.smtp.starttls.enable", true);
             props.put("mail.smtp.ssl.protocols", "TLSv1.2");
-
             props.put("mail.smtp.quit-wait", "false");
             props.put("mail.smtp.socketFactory.port", PORT);
             props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
             props.put("mail.smtp.socketFactory.fallback", "false");
-
             session = Session.getInstance(props, new Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
@@ -38,7 +38,6 @@ public class SendMail {
             System.out.println("Session Setting 실패");
         }
         return  session;
-
     }
 
     public void goMail(Session session, String title, String content){
@@ -48,9 +47,19 @@ public class SendMail {
             msg.setFrom(new InternetAddress("psy19953126@naver.com", "박선영", ENCODING));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(TO));
             msg.setSubject(title);
-            msg.setContent(content, "text/html; charset=utf-8");
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setContent(content, "text/html; charset=utf-8");
 
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            attachmentPart.attachFile("C:/Users/pc25/Desktop/J.png");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(textPart);
+            multipart.addBodyPart(attachmentPart);
+
+            msg.setContent(multipart);
             Transport.send(msg);
+
             System.out.println("메일보내기 성공");
         } catch (Exception e){
             e.printStackTrace();
